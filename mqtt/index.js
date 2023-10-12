6ac1648dbcfbaa24921423d120713f2b7cfab4a1
fiore <12385570+fioresoft@users.noreply.github.com>
2023-10-12 07:14:37 -0300

    Add files via upload

---
+++ mqtt/index.js
@@ -0,0 +1,110 @@
+const mqtt = require('mqtt')
+const express = require("express");
+const bodyParser = require("body-parser");
+//const res = require("express/lib/response");
+const app = express();
+const path = require("path");
+
+
+// Estou dizendo para o Express usar o EJS como View engine
+//app.set('view engine','html');
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// routes
+
+app.get("/",(req,res)=>{
+
+    var i = req.query.on;
+    if(i != undefined){
+        post(i);
+    }
+    var j = req.query.led;
+    if(j != undefined){
+        post(j);
+    }
+    console.log(i);
+    res.sendFile(path.join(__dirname,'views/index.html'));
+    
+});
+
+
+app.listen(80,()=>{
+    console.log("app rodando!");
+});
+
+
+/***
+    * Browser
+    * This document explains how to use MQTT over WebSocket with the ws and wss protocols.
+    * EMQX's default port for ws connection is 8083 and for wss connection is 8084.
+    * Note that you need to add a path after the connection address, such as /mqtt.
+    */
+//const url = 'ws://broker.emqx.io:8083/mqtt'
+/***
+    * Node.js
+    * This document explains how to use MQTT over TCP with both mqtt and mqtts protocols.
+    * EMQX's default port for mqtt connections is 1883, while for mqtts it is 8883.
+    */
+ //const url = 'mqtt://broker.emqx.io:1883'
+ //const url = 'mqtt://test.mosquitto.org:1883';
+ const url = "mqtt://test.mosquitto.org:1883"
+ const topic = 'fioresoft/gas';
+
+// Create an MQTT client instance
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+  // Authentication
+  clientId: 'fioresoft@454321',
+  username: '',
+  password: '',
+}
+var client  = undefined;
+client = mqtt.connect(url,options);
+
+
+if(client != undefined){
+    //client.on('connect', function () {
+    //    console.log('Connected');
+        // Subscribe to a topic
+    client.subscribe(topic);
+            
+
+    // Receive messages
+    client.on('message', function (topic, message) {
+        // message is Buffer
+        console.log("message received:" + message.toString())
+        var obj = JSON.parse(message);
+        
+        console.log(obj.time);
+        console.log(Date.now()/1000);
+        console.log(obj.time - Date.now()/1000);
+        
+        // client.end()
+    } );
+
+    function post(i)
+    {
+        var s = {
+            "signal":i,
+            "time": Date.now()
+        }
+        if(client == undefined){
+            client  = mqtt.connect(url, options);
+        }
+        console.log("publicando..." + i);
+        if(!client.connected){
+            client.reconnect();
+        }
+        // Publish a message to a topic
+        client.publish(topic, JSON.stringify(s));
+        //client.subscribe(topic);
+    }
+}
+else{
+    console.log("Failed to connect to broker");
+}
+
---
+++ mqtt_esp8266/mqtt_esp8266.ino
@@ -0,0 +1,206 @@
+/*
+ Basic ESP8266 MQTT example
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+*/
+
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>//Biblioteca do UDP.
+
+//#include <Ethernet.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+
+
+WiFiUDP udp;//Cria um objeto "UDP".
+//NTPClient ntp(udp, "a.st1.ntp.br", -3 * 3600, 60000);//Cria um objeto "NTP" com as configurações.
+NTPClient ntp(udp, "a.st1.ntp.br");//Cria um objeto "NTP" com as configurações.
+
+
+
+// Update these with values suitable for your network.
+
+const char* ssid = "Fiore";
+const char* password = "19761976";
+//const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* mqtt_server = "test.mosquitto.org";
+//const char * mqtt_server = "localhost";
+int pinSensor = A0;
+int pinBuzzer = 4; //D2
+int pinSensorDig = 5; // D1
+uint8_t pinLed = D8;
+int gas = 0;
+int dig = HIGH;
+const char *topic = "fioresoft/gas";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+//PubSubClient client(mqtt_server,(uint16_t)1883,espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+StaticJsonDocument<256> doc;
+char buffer[256];
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  //WiFi.mode(WIFI_AP);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  /*
+  for (int i = 0; i < length; i++) {
+    //Serial.print((char)payload[i]);
+    char *msg = (char *)payload;
+    msg[length] = 0;
+    Serial.println(msg);
+  }
+  */
+  // cast payload to pointer-to-const to disable ArduinoJson's zero-copy mode
+  deserializeJson(doc, (const byte*)payload, length);
+
+  uint8_t signal = doc["signal"];
+  unsigned long t = doc["time"];
+  unsigned long now = 0;
+
+  ntp.update();
+  now = ntp.getEpochTime ();
+  Serial.println(now - t);
+  // Switch on the LED if an 1 was received as first character
+  if (signal == 1) {
+    //digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    tone(pinBuzzer,1000);
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  }
+  else if(signal == 2){
+    digitalWrite(pinLed,HIGH);
+  } 
+  else if(signal == 3) {
+    digitalWrite(pinLed,LOW);
+  }
+  else if(signal == 0){
+   // digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    noTone(pinBuzzer);
+  }
+
+}
+
+void reconnect_mqtt() {
+  String clientId = "ESP8266Client-";
+  clientId += String(random(0xffff), HEX);
+  // Loop until we're reconnected
+  Serial.println("Attempting MQTT connection...");
+  while (!client.connected()) {
+    Serial.print(".");
+    if(client.connect(clientId.c_str())){
+      Serial.println("mqtt client connected");
+    }
+    else{
+      Serial.println(client.state());
+      delay(1000);
+    } 
+  }
+  client.subscribe("fioresoft/gas");
+}
+
+
+void setup()
+{
+  String clientId = "ESP8266Client-";
+  clientId += String(random(0xffff), HEX);
+  Serial.begin(9600);
+  while(!Serial)
+    continue;
+  while(!Serial.availableForWrite());
+  delay(1000);
+    Serial.println("sensor aquecendo...");
+  delay(10000);
+  //
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  setup_wifi();
+  ntp.begin();
+  ntp.forceUpdate();
+  if (client.connect(clientId.c_str())) {
+      Serial.println("ok");
+      client.subscribe(topic);
+  }
+  if(!client.connected()){
+    reconnect_mqtt();
+  }
+  pinMode(pinBuzzer, OUTPUT);
+  pinMode(pinSensor, INPUT);
+  pinMode(pinSensorDig,INPUT);
+  pinMode(pinLed,OUTPUT);
+  
+}
+
+void loop()
+{
+    gas = analogRead(pinSensor);
+    dig = digitalRead(pinSensorDig);
+    
+    ntp.update();
+    doc["time"] = ntp.getEpochTime();
+    
+    if(dig  == LOW){
+      doc["signal"] = 1;
+      
+      size_t n = serializeJson(doc, buffer);
+      client.publish(topic, buffer, n);
+      doc["signal"] = 2;
+      n = serializeJson(doc, buffer);
+      client.publish(topic, buffer, n);
+    }
+    else{
+      doc["signal"] = 0;
+      size_t n = serializeJson(doc, buffer);
+      client.publish(topic, buffer, n);
+      //client.publish(topic,"3");
+    }
+    Serial.println(gas);
+    delay(1000);
+   
+    client.loop();
+}
